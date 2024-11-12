b170b19292c2b1502097ff1edff6309ff5a3daca
lookymlive <lookymlive@gmail.com>
2024-11-12 14:36:25 -0300

    Auth whith Clerk and Nextjs V 15

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,10 @@
+export default function LayoutAuth({ children }: { children: React.ReactNode }) {
+  
+  return (
+   <div className="dlex flex-col items-center justify-items-center ">
+    <p>Auth </p>
+    <h1 className="text-3xl font-bold">Welcome to Auth</h1>
+    <h2>Auth </h2>
+    {children}
+    </div>
+)}
